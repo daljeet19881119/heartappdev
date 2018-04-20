30f490b46a8fe32d02aaf2cd30e8e27ff31e658a
Daljeet Singh <daljeet19881119@gmail.com>
2018-04-20 15:00:55 +0530

    all files changed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  // slideData array
+  slideData: any = [
+    {id: 1, currency: '10.24 K'},
+    {id: 2, currency: '250 USD'},
+    {id: 3, currency: '177.93 GBP'},
+    {id: 4, currency: '1,571.55 CNY'},
+    {id: 5, currency: '15,270.95 RUB'},
+    {id: 6, currency: '202.60 EUR'}
+  ];
 
   constructor(public navCtrl: NavController) {
 
