61c704e9031d9bf83de8024a0808156a80e51229
Daljeet Singh <daljeet19881119@gmail.com>
2018-04-27 10:22:33 +0530

    added some changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,12 +9,11 @@ export class HomePage {
   
   // slideData array
   slideData: any = [
-    {id: 1, currency: '10.24 K'},
-    {id: 2, currency: '250 USD'},
-    {id: 3, currency: '177.93 GBP'},
-    {id: 4, currency: '1,571.55 CNY'},
-    {id: 5, currency: '15,270.95 RUB'},
-    {id: 6, currency: '202.60 EUR'}
+    {id: 2, currency: '2,560.00 USD'},
+    {id: 3, currency: '1,817.59 GBP'},
+    {id: 4, currency: '1,6076.03 CNY'},
+    {id: 5, currency: '15,7127.42 RUB'},
+    {id: 6, currency: '2079.18 EUR'}
   ];
 
   constructor(public navCtrl: NavController) {
